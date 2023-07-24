e9634d65ef8979d394bc49525360cbbbec62e784
Liam Bowie <liambowie16@gmail.com>
2023-07-24 14:48:13 +0100

    Start game loop by allowing each player to place bets

--- script.py
+++ script.py
@@ -33,21 +33,41 @@ class Player:
     wager = 0
 
     def __init__(self, amount):
-        self.amount = amount
+        self.money_left = amount
 
     def __repr__(self):
-        parts = [f'Player\'s hand: ']
+        parts = [f'Hand:']
         parts.extend(f'{card}' for card in self.hand)
         parts.append(f'Wager: {self.wager}, ')
         parts.append(f'Money left: {self.amount}')
         return ' '.join(parts)
 
+    def show_hand(self):
+        parts = [f'{card}' for card in self.hand]
+        return ' '.join(parts)
+    
+    def place_bet(self, wagered):
+        if wagered < self.money_left and wagered > 0:
+            self.wager = wagered
+            self.money_left -= self.wager 
+            return self.wager
+        else:
+            return -1
+    
 class Dealer:
     hand = []
     limit = 17
 
+    def show_hand(self):
+        return f'Dealer\'s hand: ?? {self.hand[1]}'
+    
+    def reveal_hand(self):
+        parts = [f'Dealer\'s hand:']
+        parts.extend(f'{card}' for card in self.hand)
+        return ' '.join(parts)
+
     def __repr__(self):
-        parts = ['Dealer\'s hand: ']
+        parts = ['Hand:']
         parts.extend(f'{card}' for card in self.hand)
         parts.append(f'Dealer limit: {self.limit}')
         return ' '.join(parts)
@@ -65,18 +85,46 @@ class Game():
             dealer.hand.append(self.deck.draw())
         
     def __repr__(self):
-        parts = [f'{self.dealer}']
-        parts.extend(f'{player}' for player in self.players)
+        parts = [f'{self.deck}']
+        parts.append(f'Dealer\'s {self.dealer}')
+        for i, player in enumerate(self.players):
+            parts.append(f'Player {i+1}\'s {player}')
         return '\n'.join(parts)
 
-
+def sanitize(input):
+    return input.strip().lower()
 
 deck = Deck()
-player = Player(100)
+player1 = Player(100)
 dealer = Dealer()
-game = Game(deck, dealer, [player])
+game = Game(deck, dealer, [player1])
 
+# First hand starts
 deck.shuffle()
+
+for player in game.players:
+    placing_bet = True
+    wager = 0
+    while placing_bet: 
+        try:
+            wager = int(sanitize(input(f'You have {player.money_left}. How much would you like to wager?: ')))
+            wager = player.place_bet(wager)
+        except ValueError:
+            print('You must enter a number.')
+            continue
+        
+        print(wager)
+        if wager == -1:
+            print('Please bet an acceptable amount.')
+            continue
+
+        placing_bet = False
+
 game.deal_cards()
 
-print(game)
+print(dealer.show_hand())
+
+for i, player in enumerate(game.players):
+    print(f'Player {i+1}\'s {player.show_hand()}')
+
+    action = sanitize(input('Would you like to Twist, Stand, or Double Down? :'))
