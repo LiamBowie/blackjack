5450cc614607f3384d748d591b5d4374064b3e8d
Liam Bowie <liambowie16@gmail.com>
2023-07-24 15:33:09 +0100

    Control flow for players choosing actions

--- script.py
+++ script.py
@@ -6,7 +6,15 @@ class Card:
         self.suit = suit
 
     def __repr__(self):
-        return f'{self.value}{self.suit[0]}'
+        return f'{self.value} {self.suit}'
+    
+    def get_numerical_value(self):
+        if self.value == 'A':
+            return 11
+        elif self.value == 'K' or self.value == 'Q' or self.value == 'J':
+            return 10
+        else:
+            return int(self.value)
     
 class Deck:
     suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
@@ -29,11 +37,12 @@ class Deck:
         return self.cards.pop(0)
     
 class Player:
-    hand = []
-    wager = 0
 
     def __init__(self, amount):
         self.money_left = amount
+        self.hand = []
+        self.wager = 0
+        self.bust = False
 
     def __repr__(self):
         parts = [f'Hand:']
@@ -44,7 +53,10 @@ class Player:
 
     def show_hand(self):
         parts = [f'{card}' for card in self.hand]
-        return ' '.join(parts)
+        return ', '.join(parts)
+    
+    def add_card(self, card):
+        self.hand.append(card)
     
     def place_bet(self, wagered):
         if wagered < self.money_left and wagered > 0:
@@ -53,6 +65,13 @@ class Player:
             return self.wager
         else:
             return -1
+    
+    def get_hand_value(self):
+        hand_value = 0
+        for card in self.hand:
+            hand_value += card.get_numerical_value()
+
+        return hand_value
     
 class Dealer:
     hand = []
@@ -83,6 +102,10 @@ class Game():
             for player in self.players:
                 player.hand.append(self.deck.draw())
             dealer.hand.append(self.deck.draw())
+    
+    def twist(self, player):
+        player.add_card(self.deck.draw())
+        print(player.hand)
         
     def __repr__(self):
         parts = [f'{self.deck}']
@@ -96,24 +119,25 @@ def sanitize(input):
 
 deck = Deck()
 player1 = Player(100)
+player2 = Player(250)
 dealer = Dealer()
 game = Game(deck, dealer, [player1])
 
 # First hand starts
 deck.shuffle()
 
-for player in game.players:
+for i, player in enumerate(game.players):
     placing_bet = True
     wager = 0
     while placing_bet: 
         try:
-            wager = int(sanitize(input(f'You have {player.money_left}. How much would you like to wager?: ')))
-            wager = player.place_bet(wager)
+            wager = int(sanitize(input(f'Player {i+1}. You have {player.money_left}. How much would you like to wager?: ')))
+            player.wager = wager
+            wager = player.place_bet(player.wager)
         except ValueError:
             print('You must enter a number.')
             continue
-        
-        print(wager)
+
         if wager == -1:
             print('Please bet an acceptable amount.')
             continue
@@ -121,10 +145,54 @@ for player in game.players:
         placing_bet = False
 
 game.deal_cards()
+for i, player in enumerate(game.players):
+    print(f'Player {i+1}: {player.hand}')
 
 print(dealer.show_hand())
 
 for i, player in enumerate(game.players):
-    print(f'Player {i+1}\'s {player.show_hand()}')
+    
+    print(f'Player {i+1}: {player.show_hand()} {player.get_hand_value()}')
+    available_actions = ['twist', 'stand', 'double down']
 
-    action = sanitize(input('Would you like to Twist, Stand, or Double Down? :'))
+    first_turn = True
+    while player.get_hand_value() <= 21:
+
+        if player.get_hand_value() == 21:
+            print('Blackjack!')
+            break
+
+        action = sanitize(input(f'Player {i+1}. Would you like to Twist, Stand, or Double Down?: '))
+
+        if action not in available_actions:
+            print('Please enter "twist" "stand", or "double down".')
+            continue
+
+        if action == 'double down' and first_turn:
+            approved = player.place_bet(player.wager)
+
+            if approved == -1:
+                print('You do not have enough money to double down.')
+                continue
+
+            game.twist(player)
+            if player.get_hand_value() > 21:
+                print('You\'re bust')
+                player.bust = True
+            break
+
+        elif action == 'double down' and not first_turn:
+            print('You can only double down on your first action.')
+
+        if action == 'twist':
+            game.twist(player)
+            if player.get_hand_value() > 21:
+                print('You\'re bust')
+                player.bust = True
+                break
+
+        if action == 'stand':
+            break
+
+        first_turn = False
+    
