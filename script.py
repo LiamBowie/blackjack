9e8b466a38179109ee924cd3dcf52ff9de4a1d38
Liam Bowie <liambowie16@gmail.com>
2023-07-24 12:52:03 +0100

    Add deck class and populate it with card objects

--- script.py
+++ script.py
@@ -5,10 +5,20 @@ class Card:
 
     def __repr__(self):
         return f'{self.value} {self.suit}'
+    
+class Deck:
+    suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+    values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
-values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    def __init__(self):
+        self.cards = [Card(value, suit) for suit in self.suits for value in self.values]
+    
+    def __repr__(self):
+        output = []
+        for card in self.cards:
+            output.append(f'{card}')
 
-for suit in suits:
-    for value in values:
-        print(f'{Card(value, suit)}')
+        return ', '.join(output)    
+
+deck = Deck()
+print(deck)
